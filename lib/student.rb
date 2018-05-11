ab58f202e1025eb39f11673901d13061c56de3e5
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-11 04:33:39 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student
   attr_accessor :student
 
   def hello
-    "Hey there! I'm so excited to learn stuff."
+    puts "Hey there! I'm so excited to learn stuff."
   end
 
   def raise_hand
